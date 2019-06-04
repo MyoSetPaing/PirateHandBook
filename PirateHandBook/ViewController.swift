cd17296cc428a916775dba39779a60ad38a38a84
Myo Set Paing <myosetpaing@aceplussolutions.com>
2019-06-04 12:00:20 +0630

    setup beginning auto layout

---
+++ PirateHandBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PirateHandBook
+//
+//  Created by Myo Set Paing on 6/3/19.
+//  Copyright © 2019 Myo Set Paing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
